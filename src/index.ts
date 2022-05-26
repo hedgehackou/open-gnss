a691f84ab7a864cb53faa7789d7464f586959a68
Ilya Vyalkov <ilyavialkov@gmail.com>
2022-05-26 15:54:48 +0300

    add ui route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const app: Express = express();
+app.use('/', express.static(path.join(__dirname, '../build/ui')))
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../build/ui/index.html'))
+})
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
